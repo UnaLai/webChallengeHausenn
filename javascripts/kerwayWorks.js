bcaf2e889c37fadc129c9b4ff6053210f141a159
acid7820@gmail.com <acid7820@gmail.com>
2018-12-14 21:11:25 +0800

    filter做一半

--- javascripts/kerwayWorks.js
+++ javascripts/kerwayWorks.js
@@ -1,3 +1,4 @@
+
 $(function() {
   $('#MasonryLayout').imagesLoaded(function() {
     $('#MasonryLayout').masonry({
@@ -9,3 +10,37 @@ $(function() {
     });
   });
 });
+
+
+
+function showFilterText(item){
+  let strItemText = item.textContent;
+  let showTextContainer = document.querySelector('#showFilterText');
+  showTextContainer.textContent = strItemText;
+}
+
+let kerwayLists = document.querySelectorAll('#kerwayListContainer a');
+kerwayLists.forEach(function(item,indext){
+  item.addEventListener('click',function(){
+    showFilterText(item);
+  });
+
+});
+//取得 template
+let temp = document.getElementsByTagName("template")[0];
+
+//複製一份 template，cloneNode(true)
+var clon = temp.content.cloneNode(true);
+
+//放進 MasonryLayout 裡
+let masonryLayout = document.querySelector('#MasonryLayout');
+masonryLayout.appendChild(clon);
+
+let imageItems = {
+  key:{
+    name: 1,
+    photographer: 'Mango',
+    url: './images/001.jpg'
+  }
+};
+console.log(imageItems.key.name);
