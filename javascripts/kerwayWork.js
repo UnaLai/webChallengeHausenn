f197485f35fa37ca68d82e407e6e58c2e9c22aef
Askie Lin <isme408@gmail.com>
2018-12-16 13:47:37 +0800

    點擊圖片可以由右推入

---
+++ javascripts/kerwayWork.js
@@ -0,0 +1,31 @@
+var kerwayPhotoClick = (function (){
+  var masonryLayoutItem = document.getElementsByClassName('MasonryLayoutItem'),
+  wholePage = document.getElementById('wholePage'),
+  kerwayWork = document.getElementById('kerwayWork'),
+  photoContainer = document.getElementById('photoContainer'),
+  menuIcon = document.getElementById('menuIcon'),
+  controlPrevious = document.getElementById('controlPrevious'),
+  controlNext = document.getElementById('controlPrevious');
+  // console.log (masonryLayoutItem);
+
+  for (var i=0; i < masonryLayoutItem.length; i++) {
+    //全部
+    var imgBox = masonryLayoutItem[i],
+        imgSrc = imgBox.children[0].getAttribute('src');
+
+    // console.log(imgBox, imgSrc);
+    
+    imgBox.addEventListener("click", function(){
+      wholePage.classList.add('appear');
+      kerwayWork.classList.add('appear');
+      menuIcon.classList.add('appear');
+      photoContainer.src = this.children[0].getAttribute('src');
+      // console.log(this);
+    });
+  }
+
+  controlPrevious.addEventListener("click", function(){});
+  controlNext.addEventListener("click", function(){});
+
+})();
+
